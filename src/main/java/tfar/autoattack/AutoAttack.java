e41ef79f0584bda65dbad30be157c9aafb1378ad
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-07-01 11:43:04 -0400

    Update AutoAttack.java

--- src/main/java/tfar/autoattack/AutoAttack.java
+++ src/main/java/tfar/autoattack/AutoAttack.java
@@ -5,6 +5,9 @@ import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Box;
+
+import java.util.List;
 
 public class AutoAttack implements ClientModInitializer {
 
@@ -19,6 +22,13 @@ public class AutoAttack implements ClientModInitializer {
 						mc.interactionManager.attackEntity(mc.player, entity);
 					}
 				}
+			}
+			if (mc.options.advancedItemTooltips && mc.world != null && mc.world.getTime() % 15 == 0) {
+				List<Entity> entityList = mc.world.getEntities(mc.player,new Box(mc.player.getBlockPos().add(-6,-6,-6),mc.player.getBlockPos().add(6,6,6)));
+				entityList.forEach(entity -> {
+					if (entity.isAttackable())
+					mc.interactionManager.attackEntity(mc.player, entity);
+				});
 			}
 		});
 	}
